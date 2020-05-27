38c6d96e6a4c1f6e2a1a5db48195ea7561e09ae2
GudochkinK <goodok-01@hotmail.com>
2020-05-27 22:58:52 +0300

    Добавлены представления Blog и BlogSingle

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace WebStore.Controllers
     public class HomeController : Controller
     {      
         public IActionResult Index() => View();
+
         public IActionResult Blog() => View();
 
         public IActionResult BlogSingle() => View();
