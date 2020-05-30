11d5e1fc1a0537532d0eadea609a38618197f94b
GudochkinK <goodok-01@hotmail.com>
2020-05-30 19:08:09 +0300

    Определение маршрутов на уровне контроллера

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -7,6 +7,8 @@ using WebStore.Models;
 
 namespace WebStore.Controllers
 {
+    //[Route("NewRoute/[controller]/123")]
+    //[Route("Staff")]
     public class EmployeesController : Controller
     {
         private static readonly List<Employee> __Employees = new List<Employee>
@@ -37,13 +39,14 @@ namespace WebStore.Controllers
             },
         };
 
+        //[Route("{id}")]
         public IActionResult EmployeeDetails(int id)
         {
             var employee = __Employees.FirstOrDefault(e => e.Id == id);
             if (employee is null) return NotFound();
             return View(employee);
         }
-
+        //[Route("List")]
         public IActionResult Index()
         {
             return View(__Employees);
