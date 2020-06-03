dc50759876cc10cb346cecd7ced9f92aba893c4d
GudochkinK <goodok-01@hotmail.com>
2020-06-03 15:46:46 +0300

    Базовые интерфейсы сущностей

---
+++ WebStore.Domain/Entityes/Base/Interfaces/IOrderedEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Domain.Entityes.Base.Interfaces
+{
+    interface IOrderedEntity: IBaseEntity
+    {
+        int Order { get; set; }
+    }
+}
