91e52012c678e04e997a47a6e49b03121b273810
GudochkinK <goodok-01@hotmail.com>
2020-06-03 15:57:12 +0300

    Определение Секции и Бренда

---
+++ WebStore.Domain/Entityes/Section.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entityes.Base;
+
+namespace WebStore.Domain.Entityes
+{
+    class SectionNamedEntity : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+
+        public int? ParentId { get; set; }
+    }
+    
+}
