1e19a981220a70a0f8c4820fcd31fa10ff45e4be
GudochkinK <goodok-01@hotmail.com>
2020-06-10 15:55:51 +0300

    Заготовка контроллера Account

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _UserManager;
+        private readonly SignInManager<User> _SignInManager;
+
+        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
+        {
+            _UserManager = UserManager;
+            _SignInManager = SignInManager;
+        }
+
+        public IActionResult Register() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult LoginOut() => View();
+
+        public IActionResult AccessDenied() => View();
+
+    }
+}
