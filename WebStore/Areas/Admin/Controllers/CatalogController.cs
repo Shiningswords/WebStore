a05183c6cdc87eabf2ee07bef67f883c94ff5eaa
GudochkinK <goodok-01@hotmail.com>
2020-06-20 17:56:53 +0300

    Закрываем область администратора от всех, кто не обладает ролью Администратора

--- WebStore/Areas/Admin/Controllers/CatalogController.cs
+++ WebStore/Areas/Admin/Controllers/CatalogController.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebStore.Domain.Entities;
+using WebStore.Domain.Entities.Identity;
 using WebStore.Infrastructure.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    [Area("Admin"), Authorize(Roles = Role.Administrator)]
     public class CatalogController : Controller
     {
         private readonly IProductData _ProductData;
