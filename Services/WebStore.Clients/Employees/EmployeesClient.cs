214b558b1d50d711d8f9293ced7dcebc814c44e1
GudochkinK <goodok-01@hotmail.com>
2020-06-28 14:36:09 +0300

    Доработка базового клиента и заготовка клиента Employees

---
+++ Services/WebStore.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Clients.Employees
+{
+    public class EmployeesClient : BaseClient, IEmployeesData
+    {
+        public EmployeesClient(IConfiguration Configuration) : base(Configuration, WebAPI.Employees) { }
+
+        public int Add(Employee Employee)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Edit(Employee Employee)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Employee> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Employee GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
