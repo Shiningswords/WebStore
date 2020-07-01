e30669a896057f87c15a778d996ba8f6ed034e9b
GudochkinK <goodok-01@hotmail.com>
2020-07-01 14:12:44 +0300

    Заготовки контроллеров WebAPI для системы Identity

---
+++ Services/WebStore.ServiceHosting/Controllers/RolesApiController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+using WebStore.Domain;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route(WebAPI.Identity.Roles)]
+    [ApiController]
+    public class RolesApiController : ControllerBase
+    {
+        private readonly RoleStore<Role> _RoleStore;
+
+        public RolesApiController(WebStoreDb db)
+        {
+            _RoleStore = new RoleStore<Role>(db);
+        }
+
+    }
+}
---
+++ Services/WebStore.ServiceHosting/Controllers/UsersApiController.cs
@@ -0,0 +1,20 @@
+﻿using WebStore.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route(WebAPI.Identity.Users)]
+    [ApiController]
+    public class UsersApiController : ControllerBase
+    {
+        private readonly UserStore<User, Role, WebStoreDb> _UserStore;
+        public UsersApiController(WebStoreDb db)
+        {
+            _UserStore = new UserStore<User, Role, WebStoreDb>(db);
+        }
+    }
+}
