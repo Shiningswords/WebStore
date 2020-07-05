5b585d26eec10cec4cc573df4824064d33a11eb6
GudochkinK <goodok-01@hotmail.com>
2020-07-05 16:32:55 +0300

    Заготовка промежуточного ПО обработки ошибок

---
+++ UI/WebStore/Infrastructure/MiddleWare/ErrorHandlingMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace WebStore.Infrastructure.MiddleWare
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _Next;
+
+        public ErrorHandlingMiddleware(RequestDelegate Next)
+        {
+            _Next = Next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _Next(context);
+        }
+    }
+}
