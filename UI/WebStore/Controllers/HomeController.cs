8e7d05179e8b09762b830ee034f15749a959576a
GudochkinK <goodok-01@hotmail.com>
2020-07-08 21:47:46 +0300

    Пример TDD для реализации функциональности контроллера Home по обработке статусных кодов ошибок

--- UI/WebStore/Controllers/HomeController.cs
+++ UI/WebStore/Controllers/HomeController.cs
@@ -21,5 +21,17 @@ namespace WebStore.Controllers
         public IActionResult ContactUs() => View();
 
         public IActionResult Error() => View();
+
+        public IActionResult ErrorStatus(string Code)
+        {
+            switch (Code)
+            {
+                default:
+                    return Content($"Error code:{Code}");
+
+                case "404":
+                    return RedirectToAction(nameof(Error));
+            }
+        }
     }
 }
