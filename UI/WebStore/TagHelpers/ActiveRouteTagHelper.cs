1adc038ce8cd2654d54af5630b2f288272fd8afa
GudochkinK <goodok-01@hotmail.com>
2020-07-12 13:26:55 +0300

    Подсветка активной ссылки с помощью таг-хелпера

--- UI/WebStore/TagHelpers/ActiveRouteTagHelper.cs
+++ UI/WebStore/TagHelpers/ActiveRouteTagHelper.cs
@@ -5,6 +5,7 @@ using System.Security.Policy;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Linq;
 
 namespace WebStore.TagHelpers
 {
@@ -23,13 +24,49 @@ namespace WebStore.TagHelpers
         public IDictionary<string, string> RouteValues { get; set; } = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 
         [ViewContext, HtmlAttributeNotBound]
-        private ViewContext ViewContext { get; set; }
+        public ViewContext ViewContext { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-
+            if (IsActive())
+                MakeActive(output);
 
             output.Attributes.RemoveAll(AttributeName);
         }
+
+        private bool IsActive()
+        {
+            var route_values = ViewContext.RouteData.Values;
+
+            var current_controller = route_values["controller"].ToString();
+            var current_action = route_values["action"].ToString();
+
+            const StringComparison ignore_case = StringComparison.OrdinalIgnoreCase;
+            if (!string.IsNullOrEmpty(Controller) && !string.Equals(current_controller, Controller, ignore_case))
+                return false;
+
+            if (!string.IsNullOrEmpty(Action) && !string.Equals(current_action, Action, ignore_case))
+                return false;
+
+            foreach (var (key, value) in RouteValues)
+                if (!route_values.ContainsKey(key) || route_values[key].ToString() != value)
+                    return false;
+
+            return true;
+        }
+
+        private static void MakeActive(TagHelperOutput output)
+        {
+            var class_attribute = output.Attributes.FirstOrDefault(attr => attr.Name == "class");
+
+            if (class_attribute is null)
+                output.Attributes.Add("class", "active");
+            else
+            {
+                if (class_attribute.Value.ToString()?.Contains("active") ?? false) return;
+                output.Attributes.SetAttribute("class", class_attribute.Value + " active");
+            }
+        }
     }
+
 }
